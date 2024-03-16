2504c398a65347f1f809187983dd9e20a0d4cd01
Thierry Muehlemann <thierrymuhlemann@MBP.local>
2024-03-16 18:11:04 +0100

    removed unused sveltekit code

--- apps/client-sveltekit/src/lib/helper/string.ts
+++
@@ -1,6 +0,0 @@
-export function removeStartsWith(inputString, prefix) {
-    if (inputString.startsWith(prefix)) {
-        return inputString.substring(prefix.length);
-    }
-    return inputString;
-}
--- apps/client-sveltekit/src/lib/server/filesystem/indexMovies.test.ts
+++
@@ -1,6 +0,0 @@
-import {test} from "vitest";
-import {indexMovies} from "$lib/server/filesystem/indexMovies";
-
-test("basic index movies", async () => {
-    await indexMovies()
-});
